6af96c687fb06abf0774c1a85b3cfad5e2dad7b4
Sanjida Akter <sanjidakter2304@gmail.com>
2023-06-28 11:55:06 +0600

    working 3rd section

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -10,7 +10,7 @@ const Main = () => {
             <Navbar></Navbar>
             <Home></Home>
             <Second></Second>
-            {/* <Third></Third> */}
+            <Third></Third>
         </div>
     );
 };
--- src/pages/Home/Third/Third.jsx
+++ src/pages/Home/Third/Third.jsx
@@ -15,19 +15,36 @@ const Third = () => {
       >
         Presenting the technology of <br /> the future
       </h2>
-      <div className="flex ml-36">
-        <div>
+      <div className="flex gap-28 ml-36 text-white text-center mb-28">
+        <div className="mt-24">
           <div
+            className="text-center text-white"
             style={{
               borderLeft: "solid 8px blue",
               borderTop: "solid 8px blue",
               width: "275px",
               height: " 142px",
               background: "#181A25",
+              position: "relative",
+              marginBottom: "55px",
             }}
           >
-            <h2>80%</h2>
+            <h2 className="mt-8">80%</h2>
             <p>Automation o data</p>
+
+            <div
+              style={{
+                position: "absolute",
+                top: -80,
+                right: -160,
+                width: "300px",
+                height: "132px",
+                borderLeft: "solid 2px black", // border only on the left side
+                borderTop: "solid 2px black",
+                background: "transparent",
+                zIndex: "-1",
+              }}
+            ></div>
           </div>
           <div
             style={{
@@ -38,25 +55,54 @@ const Third = () => {
               background: "#181A25",
             }}
           >
-            <h2>80%</h2>
-            <p>Automation o data</p>
+            <h2 className="mt-8">50%</h2>
+            <p>Improvement in projet</p>
+            <div
+              style={{
+                position: "absolute",
+                bottom:-80,
+                right: -160,
+                width: "266px",
+                height: "132px",
+                border: "solid 4px black",
+                background: "transparent",
+              }}
+            ></div>
           </div>
         </div>
         <div>
           <img className="bg" src={robot} alt="" />
         </div>
-        <div>
+        <div className="mt-24">
           <div
+            className="text-center text-white"
             style={{
               borderLeft: "solid 8px blue",
               borderTop: "solid 8px blue",
               width: "275px",
               height: " 142px",
               background: "#181A25",
+              position: "relative",
+              marginBottom: "55px",
             }}
           >
-            <h2>80%</h2>
-            <p>Automation o data</p>
+            <h2 className="mt-8">2X</h2>
+            <p>Increase in model</p>
+
+            <div
+              style={{
+                position: "absolute",
+                top: -80,
+                left: -160,
+                width: "306px",
+                height: "132px",
+                // border only on the left side
+                borderTop: "solid 2px black",
+                borderRight: "solid 2px black",
+                background: "transparent",
+                zIndex: "-1",
+              }}
+            ></div>
           </div>
           <div
             style={{
@@ -67,11 +113,30 @@ const Third = () => {
               background: "#181A25",
             }}
           >
-            <h2>80%</h2>
-            <p>Automation o data</p>
+            <h2 className="mt-8">10X</h2>
+            <p>Reduction in errors</p>
+            <div
+              style={{
+                position: "absolute",
+                bottom: -80,
+                left: -160,
+                width: "306px",
+                height: "132px",
+                // border only on the left side
+                borderBottom: "solid 2px black",
+                borderRight: "solid 2px black",
+                background: "transparent",
+                zIndex: "-1",
+              }}
+            ></div>
           </div>
         </div>
       </div>
+      <p className="tpara text-center">
+        Artificial beings with intelligence appeared as storytelling devices in
+        antiquity,and have been common in fiction, as in Mary Shelley's
+        Frankenstein or Karel Čapek's R.U.R. These characters and
+      </p>
     </div>
   );
 };
