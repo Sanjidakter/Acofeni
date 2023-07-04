4c8d897ebddb81d7a72a80881bad5b99412eaede
Sanjida Akter <sanjidakter2304@gmail.com>
2023-07-04 21:40:28 +0600

    fourth section fixed

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -13,21 +13,15 @@ import bgimg from "../assets/bg-color.png"
 
 const Main = () => {
     return (
-        <div style={{
-            // borderRadius: "0 0 1668px 1668px",
-            // background: "linear-gradient(142deg, rgba(61, 136, 245, 0.08) 0%, rgba(217, 217, 217, 0) 100%)",
-            backgroundImage: `url(${bgimg})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-        }} >
-            {/* <Navbar></Navbar> */}
+        <div  >
+            <Navbar></Navbar>
             <Home></Home>  
-            {/* <Second></Second>  */}
-            {/* <Third></Third>
+            <Second></Second> 
+            <Third></Third>
             <Fourth></Fourth>
             <Fifth></Fifth>
             <LogoSection></LogoSection>
-            <Footer></Footer>  */}
+            <Footer></Footer> 
         </div>
     );
 };
--- src/pages/Home/Fourth/Fourth.jsx
+++ src/pages/Home/Fourth/Fourth.jsx
@@ -5,142 +5,290 @@ import "./Fourth.css";
 
 const Fourth = () => {
   return (
-    <div className="max-w-[2520px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4">
-      <div className="flex  md:felx-row ml-36 gap-40 mt-32 mb-8">
-        <div>
-          <h2
-            style={{
-              color: "var(--dark-80, #3C3C3C)",
-              fontSize: " 48px",
-              fontFamily: "Manrope",
-            }}
-          >
-            Chatboxes that fit <br /> your needs
-          </h2>
-          <p className="mb-12">
-            Artificial beings with intelligence appeared as storytelling devices
-            in antiquity,
-          </p>
-          <button
-            style={{
-              padding: "9px 35px",
-              gap: "10px",
-              borderRadius: "16px",
-              background: "var(--primary-1, #3D88F5)",
-            }}
-          >
-            Get Started
-          </button>
-          <img
-            style={{
-              width: " 529px",
-              height: " 423px",
-            }}
-            src={fimg}
-            alt=""
-          />
-        </div>
-        <div className="fs">
-          <ol type="i">
-            <li>
-              <hr />
-              <h2>
-                1{" "}
-                <span className="p-6 text-start">
-                  <span>Web AI</span> <br /> <span className="ml-12">working ex</span>
-                </span>
-              </h2>
-              <button
+    <>
+      <div className="hidden lg:grid">
+        <div className="max-w-[2520px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4">
+          <div className="flex  md:felx-row ml-36 gap-40 mt-32 mb-8">
+            <div>
+              <h2
                 style={{
-                  display: "flex",
-                  alignItems: "center",
-                  padding: "8px 16px",
-                  border: "none",
-                  marginTop: "43px",
-                  marginBottom: "43px",
+                  color: "var(--dark-80, #3C3C3C)",
+                  fontSize: " 48px",
+                  fontFamily: "Manrope",
                 }}
               >
-                Explore
-                <img
-                  src={uparrow}
-                  alt=""
-                  style={{
-                    background: "var(--primary-03, #87B4F5)",
-                    marginLeft: "8px",
-                    borderRadius: "50px",
-                    width: "25px",
-                    marginLeft: "276px",
-                    height: "25px",
-                  }}
-                />
-              </button>
-            </li>
-            <li>
-              {" "}
-              <hr />
-              <h2>
-                2 <span>Robots Learn to</span> <br /> <span className="ml-8">walk in just days</span>
+                Chatboxes that fit <br /> your needs
               </h2>
+              <p className="mb-12">
+                Artificial beings with intelligence appeared as storytelling
+                devices in antiquity,
+              </p>
               <button
                 style={{
-                  display: "flex",
-                  alignItems: "center",
-                  padding: "8px 16px",
-                  border: "none",
-                  marginTop: "43px",
-                  marginBottom: "43px",
+                  padding: "9px 35px",
+                  gap: "10px",
+                  borderRadius: "16px",
+                  background: "var(--primary-1, #3D88F5)",
                 }}
               >
-                Explore
-                <img
-                  src={uparrow}
-                  alt=""
-                  style={{
-                    background: "var(--primary-03, #87B4F5)",
-                    marginLeft: "8px",
-                    borderRadius: "50px",
-                    width: "25px",
-                    marginLeft: "276px",
-                    height: "25px",
-                  }}
-                />
+                Get Started
               </button>
-            </li>
-            <li>
-              {" "}
-              <hr />
-              <h2>
-                3 <span>Why Robots Might be the</span> <br /> <span className="ml-8">Future of work</span>
-              </h2>
-              <button
+              <img
                 style={{
-                  display: "flex",
-                  alignItems: "center",
-                  padding: "8px 16px",
-                  border: "none",
-                  marginTop: "43px",
-                  marginBottom: "43px",
+                  width: " 529px",
+                  height: " 423px",
                 }}
-              >
-                Explore
-                <img
-                  src={uparrow}
-                  alt=""
-                  style={{
-                    background: "var(--primary-03, #87B4F5)",
-                    marginLeft: "8px",
-                    borderRadius: "50px",
-                    width: "25px",
-                    marginLeft: "276px",
-                    height: "25px",
-                  }}
-                />
-              </button>
-            </li>
-          </ol>
+                src={fimg}
+                alt=""
+              />
+            </div>
+            <div className="fs">
+              <ol type="i">
+                <li>
+                  <hr />
+                  <h2>
+                    1{" "}
+                    <span className="p-6 text-start">
+                      <span>Web AI</span> <br />{" "}
+                      <span className="ml-12">working ex</span>
+                    </span>
+                  </h2>
+                  <button
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      padding: "8px 16px",
+                      border: "none",
+                      marginTop: "43px",
+                      marginBottom: "43px",
+                    }}
+                  >
+                    Explore
+                    <img
+                      src={uparrow}
+                      alt=""
+                      style={{
+                        background: "var(--primary-03, #87B4F5)",
+                        marginLeft: "8px",
+                        borderRadius: "50px",
+                        width: "25px",
+                        marginLeft: "276px",
+                        height: "25px",
+                      }}
+                    />
+                  </button>
+                </li>
+                <li>
+                  {" "}
+                  <hr />
+                  <h2>
+                    2 <span>Robots Learn to</span> <br />{" "}
+                    <span className="ml-8">walk in just days</span>
+                  </h2>
+                  <button
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      padding: "8px 16px",
+                      border: "none",
+                      marginTop: "43px",
+                      marginBottom: "43px",
+                    }}
+                  >
+                    Explore
+                    <img
+                      src={uparrow}
+                      alt=""
+                      style={{
+                        background: "var(--primary-03, #87B4F5)",
+                        marginLeft: "8px",
+                        borderRadius: "50px",
+                        width: "25px",
+                        marginLeft: "276px",
+                        height: "25px",
+                      }}
+                    />
+                  </button>
+                </li>
+                <li>
+                  {" "}
+                  <hr />
+                  <h2>
+                    3 <span>Why Robots Might be the</span> <br />{" "}
+                    <span className="ml-8">Future of work</span>
+                  </h2>
+                  <button
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      padding: "8px 16px",
+                      border: "none",
+                      marginTop: "43px",
+                      marginBottom: "43px",
+                    }}
+                  >
+                    Explore
+                    <img
+                      src={uparrow}
+                      alt=""
+                      style={{
+                        background: "var(--primary-03, #87B4F5)",
+                        marginLeft: "8px",
+                        borderRadius: "50px",
+                        width: "25px",
+                        marginLeft: "276px",
+                        height: "25px",
+                      }}
+                    />
+                  </button>
+                </li>
+              </ol>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="lg:hidden">
+        <div className="max-w-[2520px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4">
+          <div className="flex flex-col gap-10 mt-32 mb-8">
+            <div>
+              <h2
+                style={{
+                  color: "var(--dark-80, #3C3C3C)",
+                  fontSize: " 48px",
+                  fontFamily: "Manrope",
+                }}
+              >
+                Chatboxes that fit <br /> your needs
+              </h2>
+              <p className="mb-12">
+                Artificial beings with intelligence appeared as storytelling
+                devices in antiquity,
+              </p>
+              <button
+                style={{
+                  padding: "9px 35px",
+                  gap: "10px",
+                  borderRadius: "16px",
+                  background: "var(--primary-1, #3D88F5)",
+                }}
+              >
+                Get Started
+              </button>
+              <img
+                style={{
+                  width: " 529px",
+                  height: " 423px",
+                }}
+                src={fimg}
+                alt=""
+              />
+            </div>
+            <div className="fs">
+              <ol type="i">
+                <li>
+                  <hr />
+                  <h2>
+                    1{" "}
+                    <span className="p-6 text-start">
+                      <span>Web AI</span> <br />{" "}
+                      <span className="ml-12">working ex</span>
+                    </span>
+                  </h2>
+                  <button
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      padding: "8px 16px",
+                      border: "none",
+                      marginTop: "43px",
+                      marginBottom: "43px",
+                    }}
+                  >
+                    Explore
+                    <img
+                      src={uparrow}
+                      alt=""
+                      style={{
+                        background: "var(--primary-03, #87B4F5)",
+                        marginLeft: "8px",
+                        borderRadius: "50px",
+                        width: "25px",
+                        marginLeft: "276px",
+                        height: "25px",
+                      }}
+                    />
+                  </button>
+                </li>
+                <li>
+                  {" "}
+                  <hr />
+                  <h2>
+                    2 <span>Robots Learn to</span> <br />{" "}
+                    <span className="ml-8">walk in just days</span>
+                  </h2>
+                  <button
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      padding: "8px 16px",
+                      border: "none",
+                      marginTop: "43px",
+                      marginBottom: "43px",
+                    }}
+                  >
+                    Explore
+                    <img
+                      src={uparrow}
+                      alt=""
+                      style={{
+                        background: "var(--primary-03, #87B4F5)",
+                        marginLeft: "8px",
+                        borderRadius: "50px",
+                        width: "25px",
+                        marginLeft: "276px",
+                        height: "25px",
+                      }}
+                    />
+                  </button>
+                </li>
+                <li>
+                  {" "}
+                  <hr />
+                  <h2>
+                    3 <span>Why Robots Might be the</span> <br />{" "}
+                    <span className="ml-8">Future of work</span>
+                  </h2>
+                  <button
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      padding: "8px 16px",
+                      border: "none",
+                      marginTop: "43px",
+                      marginBottom: "43px",
+                    }}
+                  >
+                    Explore
+                    <img
+                      src={uparrow}
+                      alt=""
+                      style={{
+                        background: "var(--primary-03, #87B4F5)",
+                        marginLeft: "8px",
+                        borderRadius: "50px",
+                        width: "25px",
+                        marginLeft: "276px",
+                        height: "25px",
+                      }}
+                    />
+                  </button>
+                </li>
+              </ol>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
